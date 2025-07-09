84fe1efa67138cf4c288d150d74b2571373ca001
kuuakaz694 <kuua777ks@gmail.com>
2025-07-09 13:03:32 +0900

    7/9_2回目

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = "jogging-app-v1";
+const CACHE_NAME = "jog-tracker-v1";
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
   "https://cdn.jsdelivr.net/npm/chart.js"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
